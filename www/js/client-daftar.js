ebfb67be5cf855e805db17053220c991d323626b
neo alit cahya <neo.alit.cahya@live.com>
2017-08-19 10:32:18 +0700

    1. delete loader on daftar 2. change komputer to device

--- www/js/client-daftar.js
+++ www/js/client-daftar.js
@@ -5,7 +5,7 @@ function gotoRegister(){
 
 function getKota() {
 	if(globalKota.length == 0){
-		myApp.showPreloader("Mengambil Data..");
+		//myApp.showPreloader("Mengambil Data..");
 		var link=urlnya+'/api/kota/';
 		$.ajax({ dataType: "jsonp",
 		    url: link,
@@ -18,10 +18,10 @@ function getKota() {
 			{ 
 			   $('#kota_register').append( new Option(el.nama,el.id) );
 			});
-			myApp.closeModal();
+			//myApp.closeModal();
 		}).fail(function(x){
 			myApp.alert("Pengambilan data kota gagal", 'Perhatian!');
-			myApp.closeModal();
+			//myApp.closeModal();
 		}); 	
 	}else{
 		$.each(globalKota, function(i, el) 
